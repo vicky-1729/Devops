1a6b541e9130c0098c4e45d44881bcea6f5be9c5
vicky <vaikuntaraojagalingi@gmail.com>
2025-05-17 16:52:07 +0530

    modifed one

--- practice/d2.sh
+++ practice/d2.sh
@@ -1,8 +1,11 @@
 #!/bin/bash 
 NAME="vsvicky"
+
+
 echo "write the 1st fruit name bujjamma:$1"
 echo "write the 2nd fruit name bujjamma:$2"
-echo "write all products bujjamma:$*"
+echo "write all products bujjamma:$@"
 echo "count the products bujjamma:$#"
 
 ech0 "end of the project dobe bye:$NAME"
+
