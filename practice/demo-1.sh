ea22bd01a3fdf44f822c68c0ac7022713f923497
vicky <vaikuntaraojagalingi@gmail.com>
2025-05-17 16:18:10 +0530

    demo-1.ssh

--- practice/demo-1.sh
+++ practice/demo-1.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 # this is an demo one.
+echo "Hello World"
+echo "This is a demo script"
+echo "This is a demo script"
+echo "This is a demo script"
+echo "This is a demo script"
+echo "This is a demo script"
+echo "This is a demo script"
+echo "This is a demo script"
