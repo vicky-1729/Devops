5e4afaa302b4023e8490dc0ce9adbcb81e90db28
vicky <vaikuntaraojagalingi@gmail.com>
2025-05-17 16:40:50 +0530

    modifed one

---
+++ practice/d2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+#write like argumnets taking script
+$0 - name of the script
+ $1 - first argument
+ $2 - second argument
+ $3 - third argument
+ $4 - fourth argument
+ $5 - fifth argument
+ $6 - sixth argument
+ $7 - seventh argument
