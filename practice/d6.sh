970bde8cce6586c1494f89af517c66cb1171520d
vicky <vaikuntaraojagalingi@gmail.com>
2025-05-18 13:21:20 +0530

    modifed one

--- practice/d6.sh
+++ practice/d6.sh
@@ -6,5 +6,24 @@ if [ "$(id -u)" -ne 0]
 then
     echo "Please run as root"
     exit 1
+else
+    echo "You are root"
+fi
+
+dnf list installed mysql
+if [ "$?" -eq 0 ]
+then
+    echo "mysql is already installed"
+else
+    echo "mysql is not installed"
+    dnf install mysql-server -y
+
+    if [ "$?" -eq 0 ]
+    then
+        echo "mysql is installed successfully"
+    else
+        echo "mysql installation failed"
+        exit 1
+    fi
 fi
 
