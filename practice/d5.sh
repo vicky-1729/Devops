48d6d66aeaaa993ddaea0f76173098883ae7558f
vicky <vaikuntaraojagalingi@gmail.com>
2025-05-18 11:45:22 +0530

    modifed one

--- practice/d5.sh
+++ practice/d5.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 number=$1
 echo "number is $number"
-if [ number -gt 10 ]; then
+if [ $number -gt 10 ]; 
+then
     echo "number is greater than 10"
 else
     echo "number is less than 10"
