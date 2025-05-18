e2ee5bdda88489b6e1f699aff8dd4af921a05dce
vicky <vaikuntaraojagalingi@gmail.com>
2025-05-18 19:10:58 +0530

    modifed one

---
+++ practice/d7.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#check wheather the user is root or not 
+
+if [ "$(id -u)" -eq 0 ]
+then
+    echo "your are root user"
+else
+    echo "your are not a root ,use sudo or root user for exctution"
+    exit 1
+
+
