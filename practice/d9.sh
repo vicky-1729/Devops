daf3fb83fc2a344069c5eb7ca38f8f544c6b7785
vicky <vaikuntaraojagalingi@gmail.com>
2025-05-19 17:30:38 +0530

    added new text

---
+++ practice/d9.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Check whether the user is root or not
+
+if [ "$(id -u)" -eq 0 ]
+then
+    echo "You are root user"
+else
+    echo "You are not a root user, use sudo or root user for execution"
+    exit 1
+fi
+
+# Now we are writing script for package installation
+
+r="\e[31m"
+g="\e[32m"
+y="\e[33m"
+m="\e[36m"
+
+
+dnf list installed mysql
+if [ "$?" -eq 0 ]
+then
+    echo "MySQL is already $g installed, no need to do anything"
+    exit 1
+else
+    echo "MySQL is not installed, so we are going to install it"
+
+    dnf install mysql -y
+    if [ "$?" -eq 0 ]
+    then
+        echo "MySQL is installed successfully"
+    else
+        echo "MySQL installation failed"
+        exit 1
+    fi
+fi
