7923793a109343a2701ff04ad737357428af2fcf
vicky <vaikuntaraojagalingi@gmail.com>
2025-05-19 17:25:35 +0530

    added new text

--- practice/d8.sh
+++ practice/d8.sh
@@ -10,7 +10,7 @@ echo -e " \e[33m mysql\e[0m is \e[32minstalled \e[31msucesffully\e[0m bro..!"
 
 red="\e[31m"
 green="\e[32m"
-yellow ="\e[33m"
+yellow="\e[33m"
 megenta="\e[36m"
 
 echo -e "$red wipro $green is $yellow good $megenta company"
