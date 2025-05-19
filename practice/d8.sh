02d86dd24ad453ae5c6626c01616ab85a9859e13
vicky <vaikuntaraojagalingi@gmail.com>
2025-05-19 17:23:02 +0530

    added new text

--- practice/d8.sh
+++ practice/d8.sh
@@ -7,3 +7,8 @@ echo "vsvicky printig with colors"
 echo -e " your name is \e[31m vs \e[32m vicky"
 
 echo -e " \e[33m mysql\e[0m is \e[32minstalled \e[31msucesffully\e[0m bro..!"
+
+red="\e[31m"
+green="\e[32m"
+yellow ="\e[33m"
+megenta="\e[36m"
