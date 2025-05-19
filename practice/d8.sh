19b38769bd266f28bcb04aa1b118f1842f70781e
vicky <vaikuntaraojagalingi@gmail.com>
2025-05-19 17:18:29 +0530

    added new text

--- practice/d8.sh
+++ practice/d8.sh
@@ -6,4 +6,4 @@ echo "vsvicky printig with colors"
 
 echo -e " your name is \e[31m vs \e[32m vicky"
 
-echo -e " \e[33m mysql\e[0m is \e[32minstalled \e[33msucesffully\e[0m bro..!"
+echo -e " \e[33m mysql\e[0m is \e[32minstalled \e[39msucesffully\e[0m bro..!"
