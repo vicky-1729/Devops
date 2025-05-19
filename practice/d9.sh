33649f752a8addafa87abbb9e124805c9fedb714
vicky <vaikuntaraojagalingi@gmail.com>
2025-05-19 17:32:28 +0530

    added new text

--- practice/d9.sh
+++ practice/d9.sh
@@ -22,6 +22,8 @@ dnf list installed mysql
 if [ "$?" -eq 0 ]
 then
     echo -e "MySQL is already $g installed, no need to do anything"
+    echo -e "MySQL is already ${g}installed${reset}, no need to do anything"
+
     exit 1
 else
     echo "MySQL is not installed, so we are going to install it"
