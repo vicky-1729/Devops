1493d8d7868f82f7d7dc9f846507a6216ec08945
vicky <vaikuntaraojagalingi@gmail.com>
2025-06-18 15:40:25 +0530

    added new content

--- Terraform/practice/12.modules/aws_vpc_test/vpc.tf
+++ Terraform/practice/12.modules/aws_vpc_test/vpc.tf
@@ -9,8 +9,5 @@ module "vpc" {
     public_subnet_cidr = ["10.0.0.0/24","10.0.1.0/24"]
     private_subnet_cidr = ["10.0.2.0/25","10.0.2.128/25"]
     db_subnet_cidr=["10.0.3.0/25","10.0.3.128/25"]
-    private_tags= {
-        vs = "sony"
-        vs = "vicky"
-    }
+    
 }
