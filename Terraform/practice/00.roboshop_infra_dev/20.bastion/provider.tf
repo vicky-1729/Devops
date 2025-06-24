562154dd2be8216798736161bcb06b8c2a9c81fb
vicky <vaikuntaraojagalingi@gmail.com>
2025-06-25 00:21:04 +0530

    added

---
+++ Terraform/practice/00.roboshop_infra_dev/20.bastion/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.98.0"
+    }
+  }
+  backend "s3" {  
+    bucket       = "vs-roboshop-infra-dev"  
+    key          = "roboshop-infra-dev-bastion"  
+    region       = "us-east-1"  
+    encrypt      = true  
+    use_lockfile = true  #S3 native locking
+  }  
+}
+
+provider "aws"{
+    region = "us-east-1"
+}
