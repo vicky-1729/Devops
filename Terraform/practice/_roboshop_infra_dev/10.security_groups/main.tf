ca298bb27aaf47f27ede12a2bdf813b785728d59
vicky <vaikuntaraojagalingi@gmail.com>
2025-06-29 15:48:33 +0530

    added

--- Terraform/practice/_roboshop_infra_dev/10.security_groups/main.tf
+++ Terraform/practice/_roboshop_infra_dev/10.security_groups/main.tf
@@ -13,3 +13,11 @@ module "frontend"{
     Name = "${var.project}-${var.environment}-${var.sg_name}"
   }
 }
+resource "aws_security_group_rule" "frontend_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.frontend.sg_id
+}
---
+++ Terraform/practice/_roboshop_infra_dev/10.security_groups/ssm_parameter.tf
@@ -0,0 +1,11 @@
+resource "aws_ssm_parameter" "frontend_sg_id" {
+  name  = "/${var.project}/${var.environment}/frontend_sg_id"
+  type  = "String"
+  value = module.frontend.sg_id
+  
+  tags = {
+    Project     = var.project
+    Environment = var.environment
+    Purpose     = "Frontend security group ID for cross-module reference"
+  }
+}
