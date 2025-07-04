a68aa3646df718827c76f26aaa987703d3591e89
vicky <vaikuntaraojagalingi@gmail.com>
2025-07-04 15:28:49 +0530

    added

--- Terraform/practice/_roboshop_infra_dev/40.databases/bootstrap.sh
+++ Terraform/practice/_roboshop_infra_dev/40.databases/bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 dnf install ansible -y
-ansible pull -U https://github.com/daws-84s/ansible-roboshop-roles.git -e component=$1 main.yml
+ansible pull -U https://github.com/daws-84s/ansible-roboshop-roles-tf.git -e component=$1 env =$2 main.yml
