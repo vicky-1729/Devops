1f220df611f8907eebf471f3f21db37e5823e57a
vicky <vaikuntaraojagalingi@gmail.com>
2025-07-07 12:15:04 +0530

    added

--- Terraform/practice/_roboshop_infra_dev/60.catalogue/ssm_parameter.tf
+++
@@ -1,7 +0,0 @@
-# SSM Parameter for Catalogue Target Group ARN
-resource "aws_ssm_parameter" "catalogue_target_group_arn" {
-  name  = "/${var.project}/${var.environment}/catalogue_target_group_arn"
-  type  = "String"
-  value = aws_lb_target_group.catalogue.arn
-  tags  = local.common_tags
-}
